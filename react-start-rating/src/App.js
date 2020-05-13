b8a5b303a0a63cdc13dc8c7cbe313cfa278232e6
Mudita Dixit <MonicaDixit@users.noreply.github.com>
2020-05-12 17:23:09 -0700

    Add files via upload

---
+++ react-start-rating/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import StarRating from './starRating';
+
+function App() {
+  return (
+    <div className="App">
+      <StarRating></StarRating>
+    </div>
+  );
+}
+
+export default App;
